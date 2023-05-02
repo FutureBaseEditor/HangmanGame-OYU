3813447cc6b54ac3ef162ec323a292448575d0ae
AlpayCa <alpay41@gmail.com>
2023-05-03 00:33:42 +0300

    feat-add-alphabet

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adam Asmaca");
+            int score = 0;
+            Console.WriteLine("\"Puan: {0}\"",score);
         }
     }
 }
