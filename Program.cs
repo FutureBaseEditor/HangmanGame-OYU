3a3c894ade2c656a56579885ce47152907c3f675
sedat.akkus <sedat.akkus@base86.net>
2023-05-02 22:01:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HangmanGame_O_YU_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adam Asmaca");
+        }
+    }
+}
