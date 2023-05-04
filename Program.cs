37eab485c52e10312af43617520f320b14b13848
AlpayCa <alpay41@gmail.com>
2023-05-04 19:53:13 +0300

    refactor: Alphabet fixxed

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
         static void Alphabet()
         {
             char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
-            for (int harf = 0; harf < harfler.Length - 14; harf++) Console.Write("{0} ", harfler[harf]);
+            for (int harf = 0; harf < harfler.Length /2; harf++) Console.Write("{0} ", harfler[harf]);
             Console.WriteLine();
             for (int harf = 15; harf < harfler.Length; harf++) Console.Write("{0} ", harfler[harf]);
         }
