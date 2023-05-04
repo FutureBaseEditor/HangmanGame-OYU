564a9770c643a26a6fca117509e8231dd85ed640
AlpayCa <alpay41@gmail.com>
2023-05-04 15:45:00 +0300

    refactor-alphabet

--- Program.cs
+++ Program.cs
@@ -5,16 +5,14 @@
         static void Main(string[] args)
         {
             Harfyaz();
-
-          
         }
         static void Harfyaz()
         {
-            string harfler = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ";
+            char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
             for (int harf = 0; harf < harfler.Length - 14; harf++) Console.Write("{0} ", harfler[harf]);
             Console.WriteLine();
             for (int harf = 15; harf < harfler.Length; harf++) Console.Write("{0} ", harfler[harf]);
         }
-           
+
     }
 }
