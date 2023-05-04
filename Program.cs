739cc2a17a2ff34f6528ffdb90d24d8150bbe716
AlpayCa <alpay41@gmail.com>
2023-05-04 16:32:07 +0300

    refactor-the-function-name-has-been-changed.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Harfyaz();
+            Alphabet();
         }
-        static void Harfyaz()
+        static void Alphabet()
         {
             char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
             for (int harf = 0; harf < harfler.Length - 14; harf++) Console.Write("{0} ", harfler[harf]);
