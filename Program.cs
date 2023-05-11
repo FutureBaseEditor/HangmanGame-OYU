cb8350d9f9768ac0eca44721aa546054a89e4c16
Alpay Başol <alpay41@gmail.com>
2023-05-11 15:25:10 +0300

    feat: fixed

--- Program.cs
+++ Program.cs
@@ -5,8 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Adam Asmaca");
-            int score = 0;
-            Console.WriteLine("Puan: {0}",score);
+            Alphabet();
+        }
+        static void Alphabet()
+        {
+            char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
+            for (int harf = 0; harf < harfler.Length ; harf++) 
+            {
+                if (harf == harfler.Length / 2) Console.WriteLine("{0} ", harfler[harf]);
+                else Console.Write("{0} ", harfler[harf]);
+            }
+            
         }
     }
 }
