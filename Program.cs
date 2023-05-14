61801b62facbba4772d8ceba5bf6cf29e3588634
Alpay Başol <alpay41@gmail.com>
2023-05-14 17:41:37 +0300

    fead: fixed

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@
     {
         static void Main(string[] args)
         {
+            int score = 0;
+            string[] hayvanlar = { "Kedi", "Köpek", "At", "Tavşan", "Leylek", "Deve", "Yarasa", "Sincap", "Sinek", "Karınca" };
+            string[] ulkeler = { "Türkiye", "İtalya", "Kanada", "Almanya", "Japonya", "İngiltere", "Rusya", "Fransa", "Meksika", "Brezilya" };
+            string[] meslekler = { "Öğretmen", "Doktor", "Mühendis", "Hemşire", "Polis", "Avukat", "İnşaat işçisi", "Dişçi", "Gazeteci", "Bankacı" };
+            string[] yemekler = { "Makarna", "Kebap", "Pilav", "Pizza", "Köfte", "Tavuk", "Balık", "Mantı", "Börek", "Salata" };
+            string[] sporlar = { "Futbol", "Basketbol", "Voleybol", "Tenis", "Yüzme", "Atletizm", "Boks", "Jimnastik", "Hentbol", "Ragbi" };
+            string[] kategoriler = { "Hayvanlar", "Ülkeler", "Meslekler", "Yemekler", "Sporlar" };
             Console.WriteLine("Adam Asmaca");
             Alphabet();
+            ShowCategories(kategoriler);
+            Console.WriteLine("Puan: {0}",score);
         }
         static void Alphabet()
         {
@@ -15,17 +24,6 @@
                 Console.Write("{0} ", harfler[harf]);
                 if (harf == harfler.Length / 2) Console.WriteLine("{0} ", harfler[harf]);
             }
-
-            int score = 0;
-            Console.WriteLine("Puan: {0}",score);
-            string[] hayvanlar = { "Kedi", "Köpek", "At", "Tavşan", "Leylek", "Deve", "Yarasa", "Sincap", "Sinek", "Karınca" };
-            string[] ulkeler = { "Türkiye", "İtalya", "Kanada", "Almanya", "Japonya", "İngiltere", "Rusya", "Fransa", "Meksika", "Brezilya" };
-            string[] meslekler = { "Öğretmen", "Doktor", "Mühendis", "Hemşire", "Polis", "Avukat", "İnşaat işçisi", "Dişçi", "Gazeteci", "Bankacı" };
-            string[] yemekler = { "Makarna", "Kebap", "Pilav", "Pizza", "Köfte", "Tavuk", "Balık", "Mantı", "Börek", "Salata" };
-            string[] sporlar = { "Futbol", "Basketbol", "Voleybol", "Tenis", "Yüzme", "Atletizm", "Boks", "Jimnastik", "Hentbol", "Ragbi" };
-            string[] kategoriler = { "Hayvanlar", "Ülkeler", "Meslekler", "Yemekler", "Sporlar" };
-            ShowCategories(kategoriler);
-            
         }
         static void ShowCategories(string[] categories)
         {
