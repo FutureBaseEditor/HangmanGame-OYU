b0341d03cb737b33cd8aae0dd0a7ca5dda4bcc04
Alpay Başol <alpay41@gmail.com>
2023-05-16 23:37:03 +0300

    feat: fixxed

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@
             char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
             for (int harf = 0; harf < harfler.Length ; harf++) 
             {
+               Console.Write("{0} ", harfler[harf]);
                 if (harf == harfler.Length / 2) Console.WriteLine("{0} ", harfler[harf]);
-                else Console.Write("{0} ", harfler[harf]);
             }
             
         }
