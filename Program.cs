7beacdb2fe706f4e6d45dff2ee3cb6a8f51ecf1d
Alpay Başol <alpay41@gmail.com>
2023-05-19 00:04:43 +0300

    feat add refactor and fixed

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        static int wrongAnswer;
         static void Main(string[] args)
         {
 
@@ -12,166 +13,183 @@
             string[] yemekler = { "Makarna", "Kebap", "Pilav", "Pizza", "Köfte", "Tavuk", "Balık", "Mantı", "Börek", "Salata" };
             string[] sporlar = { "Futbol", "Basketbol", "Voleybol", "Tenis", "Yüzme", "Atletizm", "Boks", "Jimnastik", "Hentbol", "Ragbi" };
             string[] kategoriler = { "Hayvanlar", "Ülkeler", "Meslekler", "Yemekler", "Sporlar" };
-            
-            int wrongAnswer = 0;
-            string[] theCurrentAppearanceOfTheCharacter = { "Begingame", "Wrong1", "Wrong2", "Wrong3", "Wrong4", "Wrong5", "Wrong6" };
-            string a = theCurrentAppearanceOfTheCharacter[wrongAnswer];
 
-            Draw(a);
 
+            wrongAnswer = 5;
+                Draw();
+
+                
         }
 
-        static void Draw(string a)
-        {
-            string[] Begingame =
+            static string[] CurrentAppearance()
             {
-            "       X X X X X X 	",
-            "       X         X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                XXX "
-        };
-            string[] Wrong1 =
-            {
-            "       X X X X X X 	",
-            "       X         X	",
-            "     X X X       X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "     X X X       X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                 X	",
-            "                XXX "
-        };
-            string[] Wrong2 =
-            {
-            "       X X X X X X 	",
-            "       X         X	",
-            "     X X X       X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "     X X X       X	",
-            "       X         X	",
-            "       X         X	",
-            "       X         X	",
-            "       X         X	",
-            "       X         X	",
-            "       X         X	",
-            "                 X	",
-            "                 X	",
-            "                XXX "
-        };
-            string[] Wrong3 =
-            {
-            "       X X X X X X 	",
-            "       X         X	",
-            "     X X X       X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "     X X X       X	",
-            "       X         X	",
-            "     X X         X	",
-            "   X   X         X	",
-            "  X    X         X	",
-            "       X         X	",
-            "       X         X	",
-            "                 X	",
-            "                 X	",
-            "                XXX"
-        };
-            string[] Wrong4 =
-            {
-            "       X X X X X X 	",
-            "       X         X	",
-            "     X X X       X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "     X X X       X	",
-            "       X         X	",
-            "     X X X       X	",
-            "   X   X   X     X	",
-            "  X    X     X   X	",
-            "       X         X	",
-            "       X         X	",
-            "                 X	",
-            "                 X	",
-            "                XXX "
-        };
-            string[] Wrong5 =
-            {
-            "       X X X X X X 	",
-            "       X         X	",
-            "     X X X       X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "     X X X       X	",
-            "       X         X	",
-            "     X X X       X	",
-            "   X   X   X     X	",
-            "  X    X     X   X	",
-            "       X         X	",
-            "     X X         X	",
-            "   X             X	",
-            "  X              X	",
-            "                XXX "
-        };
-            string[] Wrong6 =
-            {
-            "       X X X X X X 	",
-            "       X         X	",
-            "     X X X       X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "    X     X      X	",
-            "     X X X       X	",
-            "       X         X	",
-            "     X X X       X	",
-            "   X   X   X     X	",
-            "  X    X     X   X	",
-            "       X         X	",
-            "     X X X       X	",
-            "   X       X     X	",
-            "  X          X   X	",
-            "                XXX "
-        };
-            string[] currentAppearance;
-            switch (a)
-            {
-                case "Wrong1":currentAppearance     = Wrong1; break;
-                case "Wrong2":currentAppearance     = Wrong2; break;
-                case "Wrong3":currentAppearance     = Wrong3; break;
-                case "Wrong4":currentAppearance     = Wrong4; break;
-                case "Wrong5":currentAppearance     = Wrong5; break;
-                case "Wrong6":currentAppearance     = Wrong6; break;
-                      default:currentAppearance     = Begingame;break;
+                if (wrongAnswer == 0)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                XXX "
+                    };
+                }
+                else if (wrongAnswer == 1)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "     X X X       X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                 X	",
+                    "                XXX "
+                    };
+                }
+                else if (wrongAnswer == 2)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "     X X X       X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "                 X	",
+                    "                 X	",
+                    "                XXX "
+                    };
+                }
+                else if (wrongAnswer == 3)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "     X X X       X	",
+                    "       X         X	",
+                    "     X X         X	",
+                    "   X   X         X	",
+                    "  X    X         X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "                 X	",
+                    "                 X	",
+                    "                XXX"
+                    };
+                }
+                else if (wrongAnswer == 4)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "     X X X       X	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "   X   X   X     X	",
+                    "  X    X     X   X	",
+                    "       X         X	",
+                    "       X         X	",
+                    "                 X	",
+                    "                 X	",
+                    "                XXX "
+                    };
+                }
+                else if (wrongAnswer == 5)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "     X X X       X	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "   X   X   X     X	",
+                    "  X    X     X   X	",
+                    "       X         X	",
+                    "     X X         X	",
+                    "   X             X	",
+                    "  X              X	",
+                    "                XXX "
+                    };
+                }
+                else if (wrongAnswer == 6)
+                {
+                    return new string[]
+                    {
+                    "       X X X X X X 	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "    X     X      X	",
+                    "     X X X       X	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "   X   X   X     X	",
+                    "  X    X     X   X	",
+                    "       X         X	",
+                    "     X X X       X	",
+                    "   X       X     X	",
+                    "  X          X   X	",
+                    "                XXX "
+                    };
+                }
+
+                return new string[0];
             }
-            for (int i = 0; i < currentAppearance.Length; i++)
+
+            static void Draw()
             {
-                Console.WriteLine(currentAppearance[i]);
+                string[] currentAppearance = CurrentAppearance();
+
+                for (int i = 0; i < currentAppearance.Length; i++)
+                {
+                    Console.WriteLine(currentAppearance[i]);
+                }
             }
         }
     }
-}
 
