b78e3c3d0c7769a1b3e16566496d2b457eb3eefd
Senan545 <senan545abbasov@gmail.com>
2023-05-28 22:20:11 +0300

    feat: add choose letter function

--- Program.cs
+++ Program.cs
@@ -19,13 +19,16 @@
 
             Console.ReadLine();
             Console.Clear();
-            
-            Console.WriteLine("Puan: {0}",score);
+
+            ChooseLetter();
+
+            Console.WriteLine("Puan: {0}", score);
         }
+
+        static char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
         static void Alphabet()
         {
-            char[] harfler = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z' };
-            for (int harf = 0; harf < harfler.Length ; harf++) 
+            for (int harf = 0; harf < harfler.Length; harf++)
             {
                 Console.Write("{0} ", harfler[harf]);
                 if (harf == harfler.Length / 2) Console.WriteLine("{0} ", harfler[harf]);
@@ -38,17 +41,50 @@
             {
                 for (int j = 0; j < columnCount; j++)
                 {
-                    if (i == 4  && j == 1) break;
-                     Console.Write("{0}- {1}\t", i + j + 1, categories[i]);
+                    if (i == 4 && j == 1) break;
+                    Console.Write("{0}- {1}\t", i + j + 1, categories[i]);
                 }
                 Console.WriteLine();
                 i++;
             }
         }
         static void ShowGameName()
-        { 
+        {
             Console.WriteLine("Adam Asmaca");
         }
-        
+
+        static void ChooseLetter()
+        {
+            string chosenLetter;
+            bool whileStop = false;
+
+            do
+            {
+                Console.Write("Bir harf seçiniz: ");
+                chosenLetter = Console.ReadLine().ToUpper();
+
+                for (int i = 0; i < harfler.Length; i++)
+                {
+                    if (chosenLetter.Length > 1 || chosenLetter.Length == 0)
+                    {
+                        Console.WriteLine("Yalnız Bir Harf Giriniz! \n");
+                        break;
+                    }
+                    else if (char.Parse(chosenLetter) == harfler[i])
+                    {
+                        Console.WriteLine("( {0} ) Harfini Seçtiniz", chosenLetter);
+                        whileStop = true;
+                        break;
+                    }
+                    else if (i == harfler.Length - 1)
+                    {
+                        Console.WriteLine("Lütfen Yalnız Harf Giriniz!  \n");
+                    }
+                }
+
+            } while (whileStop == false);
+
+        }
+
     }
 }
